d0e87aa151ad8d7ac1367756554c7b07e5798e75
klappec <cklapperichmn@gmail.com>
2025-09-21 08:54:38 -0500

    transparent labels.

--- Patches/Card3dUIGroupDisableCardBackPatch.cs
+++ Patches/Card3dUIGroupDisableCardBackPatch.cs
@@ -12,10 +12,20 @@ namespace GradedCardExpander.Patches
 
         static void Postfix(Card3dUIGroup __instance)
         {
-            // Disable the CardBackMeshBlocker renderer directly
+            // Disable the CardBackMeshBlocker renderer to prevent card back texture interference
             Transform cardBackBlocker = __instance.m_GradedCardGrp?.transform.Find("CardBackMeshBlocker");
             if (cardBackBlocker?.GetComponent<Renderer>() is Renderer renderer)
                 renderer.enabled = false;
+
+            // Disable label background images for transparent graded case support
+            if (__instance.m_GradedCardGrp != null)
+            {
+                var labelBack = __instance.m_GradedCardGrp.transform.Find("LabelImageBack")?.GetComponent<UnityEngine.UI.Image>();
+                var labelImage = __instance.m_GradedCardGrp.transform.Find("LabelImage")?.GetComponent<UnityEngine.UI.Image>();
+
+                if (labelBack != null) labelBack.enabled = false;
+                if (labelImage != null) labelImage.enabled = false;
+            }
         }
     }
 }
